c1607ee3ff4dfd3493b4f6dffee6ba0852254657
John Ross <john.ross@1904labs.com>
2017-11-30 17:04:29 -0600

    initial commit

---
+++ alphabet.js
@@ -0,0 +1,158 @@
+const alphabet = {
+    "A": {
+        "code": "* _",
+        "spoken": "DIT-DAH",
+        "audio": "sounds/A_morse_code.ogg",
+        "key": "65"
+    },
+    "B": {
+        "code": "_ * * *",
+        "spoken": "DAH-DIT-DIT-DIT",
+        "audio": "sounds/B_morse_code.ogg",
+        "key": "66"
+    },
+    "C": {
+        "code": "_ * _ *",
+        "spoken": "DAH-DIT-DAH-DIT",
+        "audio": "sounds/C_morse_code.ogg",
+        "key": "67"
+    },
+    "D": {
+        "code": "_ * *",
+        "spoken": "DAH-DIT-DIT",
+        "audio": "sounds/D_morse_code.ogg",
+        "key": "68"
+    },
+    "E": {
+        "code": "*",
+        "spoken": "DIT",
+        "audio": "sounds/E_morse_code.ogg",
+        "key": "69"
+    },
+    "F": {
+        "code": "* * _ *",
+        "spoken": "DIT-DIT-DAH-DIT",
+        "audio": "sounds/F_morse_code.ogg",
+        "key": "70"
+    },
+    "G": {
+        "code": "_ _ *",
+        "spoken": "DAH-DAH-DIT",
+        "audio": "sounds/G_morse_code.ogg",
+        "key": "71"
+    },
+    "H": {
+        "code": "* * * *",
+        "spoken": "DIT-DIT-DIT-DIT",
+        "audio": "sounds/H_morse_code.ogg",
+        "key": "72"
+    },
+    "I": {
+        "code": "* *",
+        "spoken": "DIT-DIT",
+        "audio": "sounds/I_morse_code.ogg",
+        "key": "73"
+    },
+    "J": {
+        "code": "* _ _ _",
+        "spoken": "DIT-DAH-DAH-DAH",
+        "audio": "sounds/J_morse_code.ogg",
+        "key": "74"
+    },
+    "K": {
+        "code": "_ * _",
+        "spoken": "DAH-DIT-DAH",
+        "audio": "sounds/K_morse_code.ogg",
+        "key": "75"
+    },
+    "L": {
+        "code": "* _ * *",
+        "spoken": "DIT-DAH-DIT-DIT",
+        "audio": "sounds/L_morse_code.ogg",
+        "key": "76"
+    },
+    "M": {
+        "code": "_ _",
+        "spoken": "DAH-DAH",
+        "audio": "sounds/M_morse_code.ogg",
+        "key": "77"
+    },
+    "N": {
+        "code": "_ *",
+        "spoken": "DAH-DIT",
+        "audio": "sounds/N_morse_code.ogg",
+        "key": "78"
+    },
+    "O": {
+        "code": "_ _ _",
+        "spoken": "DAH-DAH-DAH",
+        "audio": "sounds/O_morse_code.ogg",
+        "key": "79"
+    },
+    "P": {
+        "code": "* _ * _",
+        "spoken": "DIT-DAH-DIT-DAH",
+        "audio": "sounds/P_morse_code.ogg",
+        "key": "80"
+    },
+    "Q": {
+        "code": "_ _ * _",
+        "spoken": "DAH-DAH-DIT-DAH",
+        "audio": "sounds/Q_morse_code.ogg",
+        "key": "81"
+    },
+    "R": {
+        "code": "* _ *",
+        "spoken": "DIT-DAH-DIT",
+        "audio": "sounds/R_morse_code.ogg",
+        "key": "82"
+    },
+    "S": {
+        "code": "* * *",
+        "spoken": "DIT-DIT-DIT",
+        "audio": "sounds/S_morse_code.ogg",
+        "key": "83"
+    },
+    "T": {
+        "code": "_",
+        "spoken": "DAH",
+        "audio": "sounds/T_morse_code.ogg",
+        "key": "84"
+    },
+    "U": {
+        "code": "* * _",
+        "spoken": "DIT-DIT-DAH",
+        "audio": "sounds/U_morse_code.ogg",
+        "key": "85"
+    },
+    "V": {
+        "code": "* * * _",
+        "spoken": "DIT-DIT-DIT-DAH",
+        "audio": "sounds/V_morse_code.ogg",
+        "key": "86"
+    },
+    "W": {
+        "code": "* _ _",
+        "spoken": "DIT-DAH-DAH",
+        "audio": "sounds/W_morse_code.ogg",
+        "key": "87"
+    },
+    "X": {
+        "code": "_ * * _",
+        "spoken": "DAH-DIT-DIT-DAH",
+        "audio": "sounds/X_morse_code.ogg",
+        "key": "88"
+    },
+    "Y": {
+        "code": "_ * _ _",
+        "spoken": "DAH-DIT-DAH-DAH",
+        "audio": "sounds/Y_morse_code.ogg",
+        "key": "89"
+    },
+    "Z": {
+        "code": "_ _ * *",
+        "spoken": "DAH-DAH-DIT-DIT",
+        "audio": "sounds/Z_morse_code.ogg",
+        "key": "90"
+    }
+};
